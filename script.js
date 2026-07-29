ac9431e5d65a640c4f77c7d4503795ee3b4a7429
Arno <arn.hovas@gmail.com>
2026-07-29 16:10:28 +0400

    Create play round functionality with scores
    Created playRound function, checking the winner, alert results and scores.

--- script.js
+++ script.js
@@ -19,4 +19,29 @@ function getHumanChoice() {
   return choice;
 }
 
-getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    humanScore++;
+    alert(
+      `Score: ${humanScore} : ${computerScore} | You won: ${humanChoice} beats ${computerChoice}`
+    );
+  } else if (humanChoice === computerChoice) {
+    alert("It's a tie!");
+  } else {
+    computerScore++;
+    alert(
+      `Score: ${humanScore} : ${computerScore} | Computer won: ${computerChoice} beats ${humanChoice}`
+    );
+  }
+}
+
+playRound(humanSelection, computerSelection);
